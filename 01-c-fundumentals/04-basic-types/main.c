8af9e522a27b46c5e1d953dcc589f3547aa144cf
MissingCodeC <erfan.baghdadi26@gmail.com>
2026-08-06 01:30:41 +0330

    Added basic types section.

---
+++ 01-c-fundumentals/04-basic-types/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h> // Must include to use boolean type.
+
+int main ()
+{
+    int age = 22;
+    float height = 1.72f;
+    char grade = 'A';
+    char name[] = "Erfan"; // In C we don't have any string type and to declare strings we do it a an array of chars.
+    bool isOnline = true;
+
+    //TODO: cover type conversions and type definitions later.
+    return 0;
+}
