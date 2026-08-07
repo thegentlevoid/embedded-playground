f31a8c15cee38a06d1220903d44e023a3b23fe50
MissingCodeC <erfan.baghdadi26@gmail.com>
2026-08-07 21:37:29 +0330

    Updated the functions section.

--- 01-c-fundamentals/09-functions/main.c
+++ 01-c-fundamentals/09-functions/main.c
@@ -5,12 +5,23 @@ float average(float a, float b)
     return (a + b) / 2.0f;
 }
 
+// This is called a function prototype
+// If you want to define a function after the "main" function you must define it's prototype first.
+float average2(float a, float b);
+
 int main()
 {
     float num1 = 22.0f;
     float num2 = 43.0f;
     float avg = average(num1, num2);
+    float avg2 = average2(num1, num2);
     printf("The average of %.1f and %.1f is %.1f\n", num1, num2, avg);
+    printf("The average2 of %.1f and %.1f is %.1f\n", num1, num2, avg);
 
     return 0;
+}
+
+float average2(float a, float b)
+{
+    return (a + b) / 2.0f;
 }
